b78cc0331306046b368d149ada14b2e7190ac0df
KatieCodes <katiekennedy365@gmail.com>
2022-02-21 14:20:36 +0000

    Javascript for Weather conditions added

--- Weather App/src/app.js
+++ Weather App/src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
 
+  let weatherElement = document.querySelector("#conditions");
+  weatherElement.innerHTML = response.data.weather[0].description;
+
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidityData = response.data.main.humidity;
+  humidityElement.innerHTML = `Humidity: ${humidityData}%`;
+
+  let windElement = document.querySelector("#wind");
+  let speedData = Math.round(response.data.wind.speed);
+  windElement.innerHTML = `Speed: ${speedData}Km/H`;
+}
+
+let apiKey = "d23a0d3530ddedd914cb8e35a010be9d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
